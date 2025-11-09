8cad6c4c0c697e4d7bbf69797774878494d07622
Vanesa-Daria-Moisi <moisivanesa@yahoo.com>
2025-11-09 21:24:32 +0200

    final

---
+++ Migrations/LibraryIdentity/20251109191059_FinalSyncIdentity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Moisi_Vanesa_Lab2.Migrations.LibraryIdentity
+{
+    /// <inheritdoc />
+    public partial class FinalSyncIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+             }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+        }
+    }
+}
--- Pages/Borrowings/Edit.cshtml.cs
+++ Pages/Borrowings/Edit.cshtml.cs
@@ -30,14 +30,24 @@ namespace Moisi_Vanesa_Lab2.Pages.Borrowings
                 return NotFound();
             }
 
-            var borrowing =  await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+            var borrowing =  await _context.Borrowing
+                .Include(b => b.Book)
+                 .Include(b => b.Member)
+                .FirstOrDefaultAsync(m => m.ID == id);
             if (borrowing == null)
             {
                 return NotFound();
             }
             Borrowing = borrowing;
-           ViewData["BookID"] = new SelectList(_context.Book, "ID", "ID");
-           ViewData["MemberID"] = new SelectList(_context.Member, "ID", "ID");
+            var bookList = _context.Book
+        .Include(b => b.Author)
+        .Select(x => new
+        {
+            x.ID,
+            BookFullName = x.Title + " (" + x.Author.LastName + " " + x.Author.FirstName + ")"
+        });
+            ViewData["BookID"] = new SelectList(bookList, "ID", "BookFullName", Borrowing.BookID);
+            ViewData["MemberID"] = new SelectList(_context.Member, "ID", "FullName", Borrowing.MemberID);
             return Page();
         }
 
