dec2b101c68f919a35a3a284187f006179c28071
Vanesa-Daria-Moisi <moisivanesa@yahoo.com>
2025-11-09 21:40:04 +0200

    final de tot

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -41,7 +41,7 @@ namespace Moisi_Vanesa_Lab2.Areas.Identity.Pages.Account
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender,
-            Moisi_Vanesa_Lab2.Data.Moisi_Vanesa_Lab2Context context) 
+            Moisi_Vanesa_Lab2.Data.Moisi_Vanesa_Lab2Context context)
 
         {
             _userManager = userManager;
@@ -53,56 +53,28 @@ namespace Moisi_Vanesa_Lab2.Areas.Identity.Pages.Account
             _context = context;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [BindProperty]
         public Member Member { get; set; }
         [BindProperty]
         public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public string ReturnUrl { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
@@ -128,49 +100,53 @@ namespace Moisi_Vanesa_Lab2.Areas.Identity.Pages.Account
             CancellationToken.None);
             var result = await _userManager.CreateAsync(user,
             Input.Password);
-            Member.Email = Input.Email;
-            _context.Member.Add(Member);
-            await _context.SaveChangesAsync();
+
             if (result.Succeeded)
             {
+                // Codul de salvare a membrului este MUTAT AICI
+                Member.Email = Input.Email;
+                _context.Member.Add(Member);
+                await _context.SaveChangesAsync();
 
-            
-            _logger.LogInformation("User created a new account with password.");
-            var userId = await _userManager.GetUserIdAsync(user);
-            var code = await
-            _userManager.GenerateEmailConfirmationTokenAsync(user);
-            code =
-            WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-            var callbackUrl = Url.Page(
-            "/Account/ConfirmEmail",
-            pageHandler: null,
-            values: new
-            {
-                area = "Identity",
-                userId = userId,
-                code = code,
-                returnUrl = returnUrl
-            },
-            protocol: Request.Scheme);
-            await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-            $"Please confirm your account by <a href = '{HtmlEncoder.Default.Encode(callbackUrl)}' > clicking here </ a >.");
-    if
-            (_userManager.Options.SignIn.RequireConfirmedAccount)
-            {
-                return RedirectToPage("RegisterConfirmation", new
+                _logger.LogInformation("User created a new account with password.");
+                var userId = await _userManager.GetUserIdAsync(user);
+                var code = await
+                _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code =
+                WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new
                 {
-                    email = Input.Email,
+                    area = "Identity",
+                    userId = userId,
+                    code = code,
                     returnUrl = returnUrl
-                });
+                },
+                protocol: Request.Scheme);
+                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                $"Please confirm your account by <a href = '{HtmlEncoder.Default.Encode(callbackUrl)}' > clicking here </ a >.");
+
+                if
+                (_userManager.Options.SignIn.RequireConfirmedAccount)
+                {
+                    return RedirectToPage("RegisterConfirmation", new
+                    {
+                        email = Input.Email,
+                        returnUrl = returnUrl
+                    });
+                }
+                else
+                {
+                    await _signInManager.SignInAsync(user,
+                    isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
             }
-            else
-            {
-                await _signInManager.SignInAsync(user,
-                isPersistent: false);
-                return LocalRedirect(returnUrl);
-            }
-        }
-        return Page();
+            // Eroarea este returnată ca Page() doar dacă result.Succeeded este false
+
+            return Page();
         }
 
         private IdentityUser CreateUser()
---
+++ Migrations/20251109192615_MakeAddressNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Moisi_Vanesa_Lab2.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeAddressNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+        name: "Address",
+        table: "Member",
+        type: "nvarchar(max)",
+        nullable: true, // <-- Coloana devine opțională (NULL)
+        oldClrType: typeof(string),
+        oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+        name: "Address",
+        table: "Member",
+        type: "nvarchar(max)",
+        nullable: false, // <-- Coloana devine obligatorie (NOT NULL)
+        defaultValue: "", // Adaugă o valoare implicită, deși ar trebui să fie setată anterior
+        oldClrType: typeof(string),
+        oldType: "nvarchar(max)",
+        oldNullable: true);
+        }
+    }
+}
